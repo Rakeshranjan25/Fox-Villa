1faf831ddcf1cf6fd2b0eff1d8385d98104a7d75
Rakesh Ranjan Pradhan <101179634+Rakeshranjan25@users.noreply.github.com>
2023-03-05 22:08:05 +0530

    add mongodb atlas database
    5/3/23

--- api/index.js
+++ api/index.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const cors = require('cors');
+const { default: mongoose } = require('mongoose');
+require('dotenv').config()
 const app = express();
 
+
+app.use(express.json());
 app.use(cors({
     Credentials: true,
     origin:'http://127.0.0.1:5173',
    }));
 
+mongoose.connect(process.env.MONGO_URL);
+
 app.get('/test', (req,res)=> {
  res.json('test ok');
+});
+
+app.post('/register',(req,res) => {
+ const {name,email,password} = req.body;
+ res.json(name,email,password);
 });
  
 app.listen(3000);
