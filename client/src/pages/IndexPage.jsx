8b823ade6059ed3c9b269ebddbae93c01dae490f
Rakesh Ranjan Pradhan <101179634+Rakeshranjan25@users.noreply.github.com>
2023-03-05 10:32:26 +0530

    add login and register page 5/3/23
    with out api

--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -1,11 +1,8 @@
-import { Link } from "react-router-dom";
-import Header from "../Header";
-
-
+ 
 export default function IndexPage() {
   return (
     <div>
-     <Header />
+     Index Page Here
     </div>
   );
 }
